26366386a0e7192e8149c54d645d7894ad06dd46
iammanas1975 <140784033+iammanas1975@users.noreply.github.com>
2025-07-10 20:45:08 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/bank/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.bankpaymentapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, payment API is up!";
+    }
+}
